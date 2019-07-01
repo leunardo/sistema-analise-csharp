ef5d94c1e3dc335f316a29e8a09241eef0dab5ec
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-06-30 23:17:55 -0300

    feat: add data type constant

---
+++ domain/constants/DataType.cs
@@ -0,0 +1,9 @@
+namespace south.domain.constants
+{
+    public static class DataType
+    {
+        public const string Salesman = "001";
+        public const string Client   = "002";
+        public const string Sales    = "003";
+    }
+}
