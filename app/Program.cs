6b49cfda48bdac71de5946b67f7173c07a123308
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-06-30 23:03:34 -0300

    feat: startup project

---
+++ app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace south.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
