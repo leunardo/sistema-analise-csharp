bea0bc5604bb9a75f731e344a3a980fd9d543fdb
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-07-01 01:24:42 -0300

    feat: add program logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using south.application.services;
 
 namespace south
 {
@@ -6,7 +9,39 @@ namespace south
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var input = homePath + @"\data\in";
+            var extension = "*.dat";
+            var sleepTime = 10000;
+
+            while (true)
+            {
+                string[] filePaths = Directory.GetFiles(input, extension);
+                
+                foreach (var path in filePaths)
+                {
+                    var outputPath = path
+                        .Replace("in", "out")
+                        .Replace(".dat", ".done.dat");
+
+                    try
+                    {
+                        Console.WriteLine(">>> Reading file: " + path.Replace(input, ""));
+                        var data = FileService.ReadFile(path);
+
+                        Console.WriteLine("<<< Writing file: " + outputPath);
+                        FileService.WriteFile(outputPath, data);
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine("Error ocurred while writing file on " + outputPath);
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                Console.WriteLine("ZzZ Waiting " + sleepTime + "ms to scan directory again...");
+                Thread.Sleep(sleepTime);
+            }
         }
     }
 }
